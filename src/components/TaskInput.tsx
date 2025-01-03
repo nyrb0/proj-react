76a674bec8008b3cc4ec344d4be561d15ed42753
Nurbolot <nubolotkadyrov06@gmail.com>
2025-01-04 01:26:06 +0600

    deploy problem fixed

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, FC, InputHTMLAttributes, ReactNode } from "react";
+import { FC, InputHTMLAttributes, ReactNode } from "react";
 import styled from "styled-components";
 
 const InputStyled = styled.input`
